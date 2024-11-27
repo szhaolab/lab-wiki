a1306feb278c177c621a32a707794ac61cc3d794
Siming Zhao <simingz@users.noreply.github.com>
2024-11-27 15:07:18 -0500

    Update rstudio_server.sh

--- rstudio_server.sh
+++ rstudio_server.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=rstudio_server
-#SBATCH --time=29-00:00:00
-#SBATCH --mem=32G
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=20G
 #SBATCH --output=rstudio_server.txt
 #SBATCH --error=rstudio_server.err
-#SBATCH --account=nccc
+#SBATCH --account=zhao
+#SBATCH --partition=a5000
+#SBATCH --nodes=1                    # Number of nodes you require
+#SBATCH --ntasks=1                   # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=10            # Cores per task (>1 if multithread tasks)
 
 # Create the local filesystem
 TMPDIR=$HOME/rstudio-tmp
@@ -13,10 +17,16 @@ mkdir -p $TMPDIR/var/{lib,run}
 uuidgen > $TMPDIR/tmp/rstudio-server/secure-cookie-key
 chmod 600 $TMPDIR/tmp/rstudio-server/secure-cookie-key
 
+# Set OMP_NUM_THREADS to prevent OpenBLAS (and any other OpenMP-enhanced
+# libraries used by R) from spawning more threads than the number of processors
+# allocated to the job.
+export SINGULARITYENV_OMP_NUM_THREADS=${SLURM_JOB_CPUS_PER_NODE}
+
 # Don't run on the login node, but on a compute node, get the name.
 node=$(hostname -s)
 
 # Run the container
+export SINGULARITYENV_RSTUDIO_SESSION_TIMEOUT=0
 PASSWORD='aaa' singularity exec \
 --bind $TMPDIR/var/lib:/var/lib/rstudio-server \
 --bind $TMPDIR/var/run:/var/run/rstudio-server \
@@ -26,9 +36,11 @@ $HOME/rstudio.simg \
 rserver \
 --auth-none=0 \
 --auth-pam-helper-path=pam-helper \
+--auth-stay-signed-in-days=30 \
+--auth-timeout-minutes=0 \
 --www-address=${node} \
 --www-port=8789 \
 --server-user $USER
 
-# keep it alive
-sleep 3600
+# Exit
+printf 'rserver exited' 1>&2
